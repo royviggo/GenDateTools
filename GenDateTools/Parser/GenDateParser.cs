bffcd1e1f8326fc6ec760625933a97b1f8919b63
Roy Viggo Pedersen <Roy.Viggo.Pedersen@cappelendamm.no>
2021-12-06 19:50:12 +0100

    Renamed and changed static readonly to const

--- GenDateTools/Parser/GenDateParser.cs
+++ GenDateTools/Parser/GenDateParser.cs
@@ -9,22 +9,22 @@ namespace GenDateTools.Parser
             { "mai", 5 }, { "may", 5 },  { "maj", 5 },  { "mei", 5 }, { "jun", 6 }, { "jul", 7 }, { "aug", 8 }, { "sep", 9 }, {"oct", 10 },
             {"okt", 10 }, {"nov", 11 }, {"des", 12 }, {"dec", 12 }, {"dez", 12 } };
 
-        private static readonly string beginRx = "^";
-        private static readonly string endRx = "$";
-        private static readonly string sepSpaceRx = @"[\s]+";
-        private static readonly string sepIsoRx = @"-";
-        private static readonly string sepDotRx = @"\.";
-        private static readonly string dayRx = @"(?<day>\d{1,2})";
-        private static readonly string monthNumRx = @"(?<month>\d{1,2})";
-        private static readonly string monthRx = @"(?<month>" + string.Join("|", months.Keys) + ")";
-        private static readonly string yearRx = @"(?<year>\d{1,4})";
+        private const string _beginRx = "^";
+        private const string _endRx = "$";
+        private const string _sepSpaceRx = @"[\s]+";
+        private const string _sepIsoRx = @"-";
+        private const string _sepDotRx = @"\.";
+        private const string _dayRx = @"(?<day>\d{1,2})";
+        private const string _monthNumRx = @"(?<month>\d{1,2})";
+        private static readonly string _monthRx = @"(?<month>" + string.Join("|", _months.Keys) + ")";
+        private const string _yearRx = @"(?<year>\d{1,4})";
 
-        private static readonly Regex regexDayMonYear = new Regex(beginRx + dayRx + sepSpaceRx + monthRx + sepSpaceRx + yearRx + endRx);
-        private static readonly Regex regexMonYear = new Regex(beginRx + monthRx + sepSpaceRx + yearRx + endRx);
-        private static readonly Regex regexNor = new Regex(beginRx + dayRx + sepDotRx + monthNumRx + sepDotRx + yearRx + endRx);
-        private static readonly Regex regexIso = new Regex(beginRx + yearRx + sepIsoRx + monthNumRx + sepIsoRx + dayRx + endRx);
-        private static readonly Regex regexIsoMonYear = new Regex(beginRx + yearRx + sepIsoRx + monthNumRx + endRx);
-        private static readonly Regex regexYear = new Regex(beginRx + yearRx + endRx);
+        private static readonly Regex _regexDayMonYear = new Regex(_beginRx + _dayRx + _sepSpaceRx + _monthRx + _sepSpaceRx + _yearRx + _endRx);
+        private static readonly Regex _regexMonYear = new Regex(_beginRx + _monthRx + _sepSpaceRx + _yearRx + _endRx);
+        private static readonly Regex _regexNor = new Regex(_beginRx + _dayRx + _sepDotRx + _monthNumRx + _sepDotRx + _yearRx + _endRx);
+        private static readonly Regex _regexIso = new Regex(_beginRx + _yearRx + _sepIsoRx + _monthNumRx + _sepIsoRx + _dayRx + _endRx);
+        private static readonly Regex _regexIsoMonYear = new Regex(_beginRx + _yearRx + _sepIsoRx + _monthNumRx + _endRx);
+        private static readonly Regex _regexYear = new Regex(_beginRx + _yearRx + _endRx);
 
         public override GenDate Parse(string dateString)
         {
